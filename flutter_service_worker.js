2236eb87d5def772fabec89338bbb0ae96d6eb81
michael <michaelaina8@gmail.com>
2024-10-05 20:39:17 +0100

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -80,7 +80,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "44c942a72948bf4a6202383152762dac
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "d002943f1fefd9703c7735e8a0f3fa27",
+"flutter_bootstrap.js": "55e270b95ac389306a5185bee577e2c5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
